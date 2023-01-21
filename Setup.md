63061fd970512c6f6bbde434aa03e7dc281d4004
Adrian Clinansmith <adrianclinansmith@hotmail.com>
2023-01-21 17:36:58 -0500

    installed react-math and rehype-katex

--- Setup.md
+++ Setup.md
@@ -3,6 +3,9 @@
 ```bash
 $ npx create-react-app markdown-cards --template typescript
 $ npm install react-markdown
+$ npm install remark-gfm
+$ npm install remark-math
+$ npm install rehype-katex
 ```
 
 # Package Sources: 
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import "./App.css";
+import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+// import "./App.css";
+
+const mdString = `A paragraph with *emphasis* and **strong importance**.
+
+> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+* Lists
+* [ ] todo
+* [x] done
+
+A table:
+
+| a | b |
+| - | - |
+
+math: $a^2 = \\sqrt{b^2 + c^2}$
+
+C code:  
+\`\`\`c
+int myInt = 12;
+\`\`\`
+`;
 
 function App() {
 	return (
 	<div className="App">
 		<header className="App-header">
-			<ReactMarkdown># Hello, *world*!</ReactMarkdown>
+			<ReactMarkdown 
+				children={mdString}
+				remarkPlugins={[remarkGfm, remarkMath]}
+				rehypePlugins={[rehypeKatex]}
+			/>
 		</header>
 	</div>
 	);
