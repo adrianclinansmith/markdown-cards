7c499bf78bd19b7e16eb44ac5d18918dcba005ca
Adrian Clinansmith <adrianclinansmith@hotmail.com>
2023-01-22 19:32:19 -0500

    installed syntax highlighter types

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
-import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import {Prism as SyntaxHighlighter} from "react-syntax-highlighter";
 
 import 'katex/dist/katex.min.css'
 
