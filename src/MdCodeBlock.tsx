862636960f256c21be28871a292379307f181f8e
Adrian Clinansmith <adrianclinansmith@hotmail.com>
2023-01-23 13:58:00 -0500

    added CodeProps

--- src/MdCodeBlock.tsx
+++ src/MdCodeBlock.tsx
@@ -4,6 +4,8 @@
 	https://johnphung.tech/blog/adding-code-syntax-highlighting-react-markdown
 */
 
+import { CodeComponent, CodeProps } from "react-markdown/lib/ast-to-react";
+import { NormalComponents } from "react-markdown/lib/complex-types";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism"
 
@@ -12,16 +14,21 @@ type Props = {
     children: string
 }
 
-function MdCodeBlock({ language, children }: Props) {
-    return (
-        <SyntaxHighlighter
-            children={children}
-            language={language}
-			showLineNumbers={true}
-            style={materialDark}
-            wrapLines={true}
-        />
-    )
+function MdCodeBlock({node, inline, className, children, ...props}: CodeProps) {
+    const match = /language-(\w+)/.exec(className || '')
+	return !inline && match ? (
+	<SyntaxHighlighter
+		children={String(children).replace(/\n$/, '')}
+		style={vscDarkPlus}
+		language={match[1]}
+		PreTag="div"
+		{...props}
+	/>
+	) : (
+	<code className={className} {...props}>
+		{children}
+	</code>
+	)
 }
 
 export default MdCodeBlock
