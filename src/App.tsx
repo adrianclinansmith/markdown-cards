3b862d6bf0ec65a4c46285dda1a4583c2ebdbba1
Adrian Clinansmith <adrianclinansmith@hotmail.com>
2023-01-22 19:26:55 -0500

    Using Prism as syntax highlighter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from 'remark-gfm';
-import remarkMath from 'remark-math'
-import rehypeKatex from 'rehype-katex'
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
 
 import 'katex/dist/katex.min.css'
 
